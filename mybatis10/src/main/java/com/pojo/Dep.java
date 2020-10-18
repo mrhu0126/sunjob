6501bb4cc6c8f31b8888b884ea9203df61ab0da1
654240008@qq.com <654240008@qq.com>
2020-10-18 16:49:19 +0800

    sss

--- mybatis10/src/main/java/com/pojo/Dep.java
+++ mybatis10/src/main/java/com/pojo/Dep.java
@@ -6,21 +6,5 @@ public class Dep implements Serializable {
 
 
     private int did ;
-    private String dname ;
 
-    public int getDid() {
-        return did;
-    }
-
-    public void setDid(int did) {
-        this.did = did;
-    }
-
-    public String getDname() {
-        return dname;
-    }
-
-    public void setDname(String dname) {
-        this.dname = dname;
-    }
 }
